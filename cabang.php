5c403a62c8821eea09298ad20d4ff07b75722dda
Rizqi Hendra Ardiansyah <Hendrasyahardi123@gmail.com>
2022-03-29 20:01:07 +0700

    Jobsheet 6 - Desain Web

---
+++ cabang.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+    </head>
+    <body>
+        <?php
+            $nama = "Rizqi Hendra";
+
+            if($nama=="Rizqi Hendra"){
+                echo "Selamat belajar $nama";
+            }
+        ?>
+    </body>
+</html>
